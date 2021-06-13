f1775c7462631e57031ff92e6021dbc3da84579c
109598087 <t109598087@ntut.org.tw>
2021-06-13 22:47:13 +0800

    read countdown day ok

--- count_down_day_crud/read_countdown_day.py
+++ count_down_day_crud/read_countdown_day.py
@@ -3,45 +3,49 @@ import unittest
 
 from count_down_day_crud.create_countdown_day import skip_how_to_use, click_create_countdown_day_button, \
     set_countdown_day_target_day, input_countdown_day_name, set_countdown_day_countdown_book, set_countdown_day_repeat, \
-    click_save_button
+    click_save_button, get_date_of_before
 from keywords.open import appium_start_Session
 
 
 def read_countdown_day(self, countdown_day_name):
     self.driver.find_element_by_xpath(
         '//*[@class="android.widget.TextView" and contains(@text, "' + countdown_day_name + '")]').click()
-    time.sleep(1)
+
+
+def verify_read_countdown_day_successfully(self, countdown_day_name):
+    assert countdown_day_name in self.driver.find_element_by_xpath(
+        '//*[@resource-id="com.clover.daysmatter:id/title"]').get_attribute('name')
+
+
+def click_countdown_day(self):
+    self.driver.find_element_by_xpath(
+        '//*[@resource-id="com.clover.daysmatter:id/date"]')
+
+
+def verify_change_read_date_format_successfully(self):
+    print(self.driver.find_element_by_xpath(
+        '//*[@resource-id="com.clover.daysmatter:id/date"]').get_attribute('name'))
+    assert '天' in self.driver.find_element_by_xpath(
+        '//*[@resource-id="com.clover.daysmatter:id/date"]').get_attribute('name')
 
 
 class TestCountDownRead(unittest.TestCase):
     def setUp(self) -> None:
         appium_start_Session(self)
         skip_how_to_use(self)
+        # create a_countdown_day
+        click_create_countdown_day_button(self)
+        self.countdown_day_name = 'countdown_day_name'
+        input_countdown_day_name(self, self.countdown_day_name)
+        click_save_button(self)
 
     def test_read_countdown_day(self):
-        click_create_countdown_day_button(self)
-        # 輸入到數日名稱
-        countdown_day_name = '123456789'
-        countdown_day_target_year = 2020
-        countdown_day_target_month = 5
-        countdown_day_target_day = 8
-        countdown_day_countdown_book = '工作'
-        countdown_day_repeat = 'Months'
-
-        input_countdown_day_name(self, countdown_day_name)
-        set_countdown_day_target_day(self, countdown_day_target_year, countdown_day_target_month,
-                                     countdown_day_target_day)
-        set_countdown_day_countdown_book(self, countdown_day_countdown_book)
-        # click_countdown_day_set_top_button(self)
-        set_countdown_day_repeat(self, countdown_day_repeat)
-        # 保存
-        click_save_button(self)
         # read
-        read_countdown_day(self, countdown_day_name)
-        assert countdown_day_name in self.driver.find_element_by_xpath(
-            '//*[@resource-id="com.clover.daysmatter:id/title"]').get_attribute('name')
+        read_countdown_day(self, self.countdown_day_name)
+        verify_read_countdown_day_successfully(self, self.countdown_day_name)
+        # click_countdown_day(self)
+        # verify_change_read_date_format_successfully(self)
         print('test_read_countdown_day ok')
 
     def tearDown(self) -> None:
         self.driver.quit()
-
